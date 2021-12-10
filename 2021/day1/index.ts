87ce3f7781bf82996c7b6971481c0970b3bc2c4f
Michael Abrahamian <michael.abrahamian7@gmail.com>
2021-12-10 17:25:12 +1100

    feat: complete day 1 part 1

--- 2021/day1/index.ts
+++ 2021/day1/index.ts
@@ -1,5 +1,24 @@
+import fs from 'fs';
+
 // place input into an array
 // loop through input, starting at index 1
 // check if current index value is greater than previous
+const main = () => {
+  const fileContents = fs.readFileSync(`${__dirname}/input`, 'utf8')
+  const depthMeasurements = fileContents.split('\n');
 
-console.log('Welcome!')
+  let numberOfDepthMeasurementIncreases = 0;
+
+  for (let i = 1; i < depthMeasurements.length; i++) {
+    const currentDepth = parseInt(depthMeasurements[i]);
+    const previousDepth = parseInt(depthMeasurements[i - 1]);
+
+    if (currentDepth > previousDepth) {
+      numberOfDepthMeasurementIncreases++;
+    }
+  }
+
+  console.log('numberOfDepthMeasurementIncreases', numberOfDepthMeasurementIncreases);
+}
+
+main();
